4bce222d47206f85537fb3d5e4e525103fb3bcbe
Pedro V Pinho <pvpedrinho06@gmail.com>
2023-05-28 23:04:40 -0300

    Added file read/write functionality

--- src/main.cpp
+++ src/main.cpp
@@ -5,6 +5,9 @@
 
 #include <iostream>
 #include <string>
+#include <fstream>
+#include <sstream>
+
 #include "Node.hpp"
 #include "LinkedList.hpp"
 #include "Song.hpp"
@@ -67,17 +70,108 @@ void setup(LinkedList<Song> &songs, LinkedList<Playlist> &playlists){
     std::cin.get();
 }
 
-int main(){
+/**
+ * @brief Lê o arquivo e adiciona as playlists e músicas ao sistema.
+ * 
+ * @param in_file Arquivo de entrada.
+ * @param songs Lista encadeada de músicas.
+ * @param playlists Lista encadeada de playlists.
+ */
+void parseFile(std::ifstream& in_file, LinkedList<Song>& songs, LinkedList<Playlist> &playlists){
+    std::string line;
+    while(std::getline(in_file, line)){
+        std::istringstream iss{line};
+        std::string pl_title;
+        std::getline(iss, pl_title, ';');
+        
+        Playlist playlist{pl_title};
+        playlists.add(playlist);
+
+        std::string song_string;
+        while(std::getline(iss, song_string, ',')){
+            std::istringstream iss_song{song_string};
+            std::string song_title;
+            std::string song_author;
+
+            std::getline(iss_song, song_title, ':');
+            std::getline(iss_song, song_author);
+
+            Song song{song_title, song_author};
+            songs.add(song);
+
+            playlists.searchValue(playlist)->addSong(song);
+        }
+    }
+}
+
+/**
+ * @brief Escreve as playlists e músicas do sistema ao arquivo.
+ * 
+ * @param out_file Arquivo de saída.
+ * @param songs Lista encadeada de músicas.
+ * @param playlists Lista encadeada de playlists.
+ */
+void writeFile(std::ofstream& out_file, LinkedList<Song>& songs, LinkedList<Playlist> &playlists){
+    auto plRunner = playlists.getHead();
+
+    while(plRunner != nullptr){
+        auto currPl = plRunner->getValue();
+
+        out_file << currPl.getName() << ';';
+
+        auto songRunner = currPl.getSongs().getHead();
+
+        while(songRunner != nullptr){
+            auto currSong = songRunner->getValue();
+
+            out_file << currSong.getTitle() << ':';
+            out_file << currSong.getAuthor();
+
+            if(songRunner->getNext() != nullptr){
+                out_file << ',';
+            }
+
+            songRunner = songRunner->getNext();
+        }
+
+        out_file << '\n';
+        plRunner = plRunner->getNext();
+    }
+}
+
+int main(int argc, char *argv[]){
     LinkedList<Playlist> playlists;
     LinkedList<Song> songs;
+
+    if(argc != 2){
+        std::cout << "Número de argumentos inválido! Esperado 1 argumento (nome do arquivo).\n";
+        return 1;
+    }
     
-    setup(songs, playlists);
+    std::ifstream in_file;
+    in_file.open(argv[1]);
+
+    if(!in_file.is_open()){
+        std::cout << "Não foi possível abrir o arquivo \"" << argv[1] << "\"!\n";
+        return 1;
+    }
+    
+    parseFile(in_file, songs, playlists);
+
+    in_file.close();
 
     int exit{0};
 
     while(exit == 0){
         exit = mainMenu(songs, playlists);
     }
+
+    std::ofstream out_file;
+    out_file.open(argv[1]);
+
+    writeFile(out_file, songs, playlists);
+
+    out_file.close();
 
     playlists.clear();
     songs.clear();
