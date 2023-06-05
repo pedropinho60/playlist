60dc83d9fb7a3885f8c6124eb1ff098d882d8070
Pedro V Pinho <pvpedrinho06@gmail.com>
2023-06-05 20:56:47 -0300

    Updated Doxygen documentation

--- docs/html/search/all_8.js
+++ docs/html/search/all_8.js
@@ -6,7 +6,7 @@ var searchData=
   ['playlist_2ehpp_3',['Playlist.hpp',['../Playlist_8hpp.html',1,'']]],
   ['playlistmenu_4',['playlistMenu',['../menu_8hpp.html#ae476d435a88302b800f88ce6a2b874ed',1,'playlistMenu(LinkedList&lt; Playlist &gt; &amp;playlists):&#160;menu.cpp'],['../menu_8cpp.html#ae476d435a88302b800f88ce6a2b874ed',1,'playlistMenu(LinkedList&lt; Playlist &gt; &amp;playlists):&#160;menu.cpp']]],
   ['playsongs_5',['playSongs',['../menu_8hpp.html#aca1c026897221daf62bf7bc0f687148e',1,'playSongs(LinkedList&lt; Playlist &gt; &amp;playlists):&#160;menu.cpp'],['../menu_8cpp.html#aca1c026897221daf62bf7bc0f687148e',1,'playSongs(LinkedList&lt; Playlist &gt; &amp;playlists):&#160;menu.cpp']]],
-  ['popback_6',['popBack',['../classLinkedList.html#ad383b9db98a8b1f81c46368d62b90750',1,'LinkedList']]],
+  ['popback_6',['popBack',['../classLinkedList.html#aeb85f62afcce2e37f236cd915c4194ff',1,'LinkedList']]],
   ['print_7',['print',['../classLinkedList.html#a9675767b81fb9f1d8799444e4ee7f43b',1,'LinkedList']]],
   ['printaux_8',['printAux',['../LinkedList_8hpp.html#ae9b172f645976243d2370d7ed801e279',1,'LinkedList.hpp']]],
   ['printsongs_9',['printSongs',['../classPlaylist.html#a352cd7aa99543baffb7aa2466d71bd96',1,'Playlist']]],
